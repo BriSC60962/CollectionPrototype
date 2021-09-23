6d5c1e24460dcb19c5ed5e6f021a076bd4c5d59d
BriSC60962 <70978684+BriSC60962@users.noreply.github.com>
2021-09-23 19:18:42 -0300

    Changed color of floors to make it feel less like a maze game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -41,7 +41,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 22)
+        if (count >= 23)
         {
             winTextObject.SetActive(true);
         }
