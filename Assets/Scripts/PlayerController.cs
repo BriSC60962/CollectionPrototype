2749ec34a3027f79ff63cf1a7bdee1aec06a3b47
BriSC60962 <70978684+BriSC60962@users.noreply.github.com>
2021-10-22 14:37:03 -0300

    Added New Game Prototype Scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 using TMPro;
 
 
@@ -13,15 +13,14 @@ public class PlayerController : MonoBehaviour
     public GameObject winTextObject;
 
 
-    private Rigidbody rb;
+    
     private int count;
-    private float movementX;
-    private float movementY;
+    
 
     // Start is called before the first frame update
     void Start()
     { 
-        rb = GetComponent <Rigidbody>();
+        
         count = 0;
 
 
@@ -30,29 +29,18 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
+   
 
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 23)
+        if (count >= 3)
         {
             winTextObject.SetActive(true);
         }
     }
 
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
-        rb.AddForce(movement * speed);
-    }
+   
 
     private void OnTriggerEnter(Collider other)
     {
